b780bbb55f4332a4f18fc1cb0dd4de0417926a12
ebachoura <edlobacro@gmail.com>
2018-05-04 21:27:07 -0700

    updated website with trailze and olive

--- script.js
+++ script.js
@@ -10,7 +10,13 @@
 
   let projects = [
     {
-      name: "New York Times API Project",
+      name: "olive",
+      description: "A high-level, high-performance language for numerical computing which compiles down to Javascript.",
+      image: "resources/olive-cover.png",
+      url: "https://eileenchoe.github.io/olive/website/index.html"
+    },
+    {
+      name: "Interaction Design API Project",
       description: "For my Interaction Design class, we were asked to put together a front end for a pre-existing web service API. I chose the New York Times API and this was the front end that I developed using it.",
       image: "resources/thenewyorktimes-cover.png",
       url: "https://ebachoura.github.io/newyorktimes"
@@ -20,16 +26,22 @@
       description: "Part of development team which researches and renders data visualizations of gene regulatory networks (GRNs) using this web application that we have developed.",
       image: "resources/grnsight-cover.png",
       url: "https://dondi.github.io/GRNsight"
-    }
+    },
+    {
+      name: "trailze",
+      description: "On the front-end development team for this application which aims to create an autonomous community of hikers who solve problems and provide information as they adventure.",
+      image: "resources/trailze-cover.png",
+      url: "https://github.com/cpetrosi/trailze"
+    },
   ];
 
 
   let insertProjects = (projects) => {
     let toAppend = '';
-    projects.forEach((project) => {
+    projects.forEach((project, index) => {
       toAppend += '<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12"' +
-      'style="text-align:center;">' +
-      '<a style="display:inline-block; width: 70%;" href="' + project.url + '">' +
+        'style="text-align:' + ((index % 2 !== 0) ? 'left;">' : 'right;">') +
+      '<a style="display:inline-block; width: 69%;" href="' + project.url + '">' +
       '<div class="project">' +
       '<img class="project-image" src="' + project.image +'"/>' +
       '<div class="overlay">' +
